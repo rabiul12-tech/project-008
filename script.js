15f5efca504a49a2ff26fd741fa5c41983eaa4ac
rabiul12-tech <rabiulalamin@gmail.com>
2025-10-11 19:03:55 +0600

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+// Project 8 minimal JS
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("actionBtn");
+  if (btn) {
+    btn.addEventListener("click", () => {
+      alert("Hello from Project 8! 🎉");
+    });
+  }
+});
